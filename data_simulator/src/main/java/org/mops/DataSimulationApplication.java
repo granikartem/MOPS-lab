adc6edbb001cbb7280fe734f133c23cc04ac73e0
a.granik <a.granik@just-ai.com>
2025-01-18 19:40:06 +0300

    Subprojects basic dependencies.

---
+++ data_simulator/src/main/java/org/mops/DataSimulationApplication.java
@@ -0,0 +1,11 @@
+package org.mops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataSimulationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DataSimulationApplication.class, args);
+    }
+}
---
+++ iot_controller/src/main/java/org/mops/IoTControllerApplication.java
@@ -0,0 +1,11 @@
+package org.mops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTControllerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IoTControllerApplication.class, args);
+    }
+}
